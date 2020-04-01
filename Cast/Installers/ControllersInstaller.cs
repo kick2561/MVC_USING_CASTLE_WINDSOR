d674a54455a08287cbbbcec8e7cf85580e5ea065
Bonu <cb185249@ncr.com>
2020-04-01 14:54:48 +0530

    MVC using castle windsor

---
+++ Cast/Installers/ControllersInstaller.cs
@@ -0,0 +1,29 @@
+using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Cast.Installers
+{
+    using Cast.Controllers;
+    using Cast.Models;
+    using Plumbing;
+
+    public class ControllersInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            
+            container.Register(
+                Classes.
+                    FromThisAssembly().
+                    BasedOn<IController>().
+                    If(c => c.Name.EndsWith("Controller")).
+                    LifestyleTransient());
+
+            ControllerBuilder.Current.SetControllerFactory(new WindsorControllerFactory(container));
+       
+            
+        }
+    }
+}
---
+++ Cast/Installers/ServiceInstaller.cs
@@ -0,0 +1,22 @@
+﻿using Cast.Models;
+using Castle.MicroKernel.Registration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cast.Installers
+{
+    public class ServiceInstaller : IWindsorInstaller
+    {
+        public void Install(Castle.Windsor.IWindsorContainer container,
+    Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            container.Register(
+                Component
+                    .For<StudentDetailsEntities>()
+                    .ImplementedBy<StudentDetailsEntities>()
+                    .LifestyleTransient());
+        }
+    }
+}
